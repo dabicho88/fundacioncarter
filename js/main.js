ec8e1fdb1bcf30b4aafbf4e2da404d0b0a28039d
carter88 <dabicho88@hotmail.com>
2025-07-13 04:53:57 -0600

    eliminado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const URL = 'https://i5.walmartimages.com/dfw/4ff9c6c9-ccd3/k2-_71d75f0a-fcaa-4eab-9cc2-9aba95fdcda9.v1.json';
+const URL = 'js/catalogofstreetdogs.json';
 const contenedorPrincipal = document.querySelector('.tuSeleccion');
 
 
