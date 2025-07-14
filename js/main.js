727902173403983016ae16956dfcc0bfa8b19eb7
carter88 <dabicho88@hotmail.com>
2025-07-14 17:53:26 -0600

    actualiza json

--- js/main.js
+++ js/main.js
@@ -1,29 +1,44 @@
-//const URL = 'https://github.com/dabicho88/fundacioncarter/blob/main/js/catalogofstreetdogs.json';
-const URL = 'js/catalogofstreetdogs.json';
+const URL = 'https://dabicho88.github.io/fundacioncarter/js/catalogofstreetdogs.json';
+//const URL = 'js/catalogofstreetdogs.json';
 const contenedorPrincipal = document.querySelector('.tuSeleccion');
-
+let foto;
+let nombre;
+let tamanno;
+let color;
+let pelaje;
+let edad;
+let ciudad;
+let favorito;
+let numHeart = 0;
+let jsonEnStorage;
+class Perro{
+    constructor(foto, nombre, tamanno, color, pelaje, edad, ciudad){
+        this.foto = foto;
+        this.nombre = nombre;
+        this.tamanno = tamanno;
+        this.color = color;
+        this.pelaje = pelaje;
+        this.edad = edad;
+        this.ciudad = ciudad;
+    }
+}
 
 function comienzaPeticion(){
     fetch(URL)
         .then((laData) => laData.json())
         .then((losDatos) => {
             generaFichas(losDatos);
+            leerFavoritos(losDatos, jsonEnStorage);
         });
-}
-
-function generaFormulario(corazon){
-    corazon.addEventListener(() => {
-        console.log(corazon.parentNode);
-    });
 }
 
 function generaFichas(allData){
     allData.forEach(oneDog => {
-        let del1al4 = Math.floor(Math.random()* 4 + 1);
-        const everycard = document.createElement('div');
-        everycard.classList.add('everycard');  
+        everycard = document.createElement('div');
+        everycard.classList.add('everycard'); 
+        numHeart++;
         everycard.innerHTML = `            
-            <div class="picCard"><span class="outfit-2">${oneDog.nombre}</span> <img src="img/carter${oneDog.color == 'Blanco' ? 'White' : (oneDog.color == 'Negro' ? 'Black' : (oneDog.color == 'Miel' ? 'Beige' : (oneDog.color == 'Mixto' ? 'Mixto' : 'noexiste')))}${del1al4}.jpg"> </div>
+            <div class="picCard"><span class="outfit-2">${oneDog.nombre}</span> <img src="${oneDog.foto}"> </div>
             <div class="vueltaCard">
                 <div class="bulletCard">Mi nombre es: <span class="titleCard">${oneDog.nombre}</span></div>
                 <div class="bulletCard">Tamaño: <span class="titleCard">${oneDog.size}</span></div>
@@ -31,15 +46,62 @@ function generaFichas(allData){
                 <div class="bulletCard">Mi pelaje es: <span class="titleCard">${oneDog.pelaje}</span></div>
                 <div class="bulletCard">Tengo: <span class="titleCard">${oneDog.edad} años</span></div>
                 <div class="bulletCard">Localidad: <span class="titleCard">${oneDog.ciudad}</span></div>
-                <div class="heart">♥</div>
+                <div class="heart" id="favorito${numHeart}" onclick="saveFavorito(this)">♥</div>
             </div>
         `;
-
         contenedorPrincipal.insertAdjacentElement('beforeend', everycard);
     });
 }
 
+function saveFavorito(corasao){
+    let idFavorito = corasao.getAttribute('id');
+    favorito = new Perro(
+            corasao.parentNode.previousElementSibling.children[1].getAttribute('src'),
+            corasao.parentNode.children[0].children[0].innerHTML,
+            corasao.parentNode.children[1].children[0].innerHTML,
+            corasao.parentNode.children[2].children[0].innerHTML,
+            corasao.parentNode.children[3].children[0].innerHTML,
+            corasao.parentNode.children[4].children[0].innerHTML,
+            corasao.parentNode.children[5].children[0].innerHTML
+        );
+    //Almacenando en local storage
+    localStorage.setItem(idFavorito, JSON.stringify(favorito));
+    let objecObtenido = JSON.parse(localStorage.getItem("'"+idFavorito+"'"));
+    console.log(objecObtenido);
+}
+
+function leerFavoritos(){
+    for(){
+
+    }
+}
 
 
 comienzaPeticion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
